bcb53fd91dd3006ecb5eb4e520b70fb86b2d4cec
khs1994 <khs1994@khs1994.com>
2020-06-29 23:51:02 +0800

    Sync from pcit-ce/pcit@bf6bdc5b5572637f89a3cc7015da93cd296a7afb by PCIT

--- src/Webhooks/Handler/Push.php
+++ src/Webhooks/Handler/Push.php
@@ -4,15 +4,32 @@ declare(strict_types=1);
 
 namespace PCIT\Coding\Webhooks\Handler;
 
-use PCIT\GPI\Webhooks\Handler\PushAbstract;
+use PCIT\Coding\Webhooks\Parser\Push as PushParser;
+use PCIT\GPI\Webhooks\Context\TagContext;
+use PCIT\GPI\Webhooks\Handler\Abstracts\PushAbstract;
+use PCIT\GPI\Webhooks\Handler\DisableHandler;
 
 class Push extends PushAbstract
 {
+    public $git_type = 'coding';
+
     public function handle(string $webhooks_content): void
     {
+        $context = PushParser::handle($webhooks_content);
+
+        DisableHandler::handle($context->repo_full_name, $this->git_type);
+
+        if ($context->tag ?? null) {
+            $this->tag($context);
+
+            return;
+        }
+
+        $this->handlePush($context, $this->git_type);
     }
 
-    public function tag(array $content): void
+    public function tag(TagContext $context): void
     {
+        $this->handleTag($context, $this->git_type);
     }
 }
--- src/Webhooks/Parser/Push.php
+++ src/Webhooks/Parser/Push.php
@@ -4,6 +4,19 @@ declare(strict_types=1);
 
 namespace PCIT\Coding\Webhooks\Parser;
 
-class Push
+use PCIT\GitHub\Webhooks\Parser\Push as GitHubPushParser;
+use PCIT\GPI\Webhooks\Context;
+
+class Push extends GitHubPushParser
 {
+    use Common;
+
+    public static function handle(string $webhooks_content): Context
+    {
+        $context = parent::handle($webhooks_content);
+
+        $context->repo_full_name = self::handle_repo_full_name($context->repo_full_name);
+
+        return $context;
+    }
 }
